0c452a5c06cdd3603714cac900aa32bfdd5eceb6
Abdullah <abd.247@hotmail.com>
2023-10-05 16:39:49 -0400

    Initial Commit

---
+++ DiabudiOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DiabudiOS
+//
+//  Created by Abdullah Shahid on 2023-10-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiabudiOS/DiabudiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiabudiOSApp.swift
+//  DiabudiOS
+//
+//  Created by Abdullah Shahid on 2023-10-05.
+//
+
+import SwiftUI
+
+@main
+struct DiabudiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
